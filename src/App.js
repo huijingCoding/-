f84cb91f075daf647fe2c4789fc3bc02703df824
xzl123z <1449240911@qq.com>
2020-03-27 14:08:22 +0800

    项目启动

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter, Route, } from 'react-router-dom'
+import Login from './pages/Login/index'
+import Admin from './pages/Admin/index'
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/admin" render={() => { 
+          return (
+            <Admin>
+               {/* 路由器 */}
+              
+            </Admin>
+          )
+        }}></Route>
+      
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
+
---
+++ src/pages/Admin/index.js
@@ -0,0 +1,16 @@
+import React, { Fragment,Component} from 'react'
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <Fragment>
+                首页
+            </Fragment>
+        );
+    }
+}
+
+export default Login;
